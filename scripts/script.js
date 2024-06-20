0ba13bffed80f47eb0ea317723935b0ade01be41
Amilton Gove <amiltoncpro@gmail.com>
2024-06-20 21:48:58 +0200

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// Menu responsivo
+var a = window.document.getElementsByClassName('humburguer')[0]
+var b = window.document.getElementsByClassName('menu')
+
+a.addEventListener('click', menu)
+
+function menu() {
+    if (b[0].style.display == 'none') {
+        b[0].style.display = 'block'
+    } else {
+        b[0].style.display ='none'
+    }
+}
+
+function tamanho() {
+    if (innerWidth >= 768) {
+        b[0].style.display = 'flex'
+    } else {
+        b[0].style.display ='none'
+    }
+}
+// -----------------------------------------------------------------------------------
